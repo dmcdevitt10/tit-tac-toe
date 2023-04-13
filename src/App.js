aa26befacb8008a2a41f9a9cb8c35d02da9a409c
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-04-13 17:16:01 -0600

    mapped squares array and added onClicks

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
 
+  const handleClick = () => {
+    setSquares(['', '', '', '', '', '', '', '', ''])
+    setPlayer(true)
+  }
+
   return (
     <div className="App">
-      <Square
-        squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer}
-      />
+      <button onClick={handleClick}>Reset</button>
+      <div className="container">
+        {squares.map((value, index) => (
+          <Square
+            squares={squares}
+            setSquares={setSquares}
+            player={player}
+            setPlayer={setPlayer}
+            squareValue={value}
+            index={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
